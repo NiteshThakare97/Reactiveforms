b328b6bb55aa1c3b5fc3ac044bb2a221632c5560
Nitesh Thakare <nthakare09@gmail.com>
2023-06-09 23:05:52 +0530

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -13,17 +14,45 @@ export class AppComponent implements OnInit{
   ngOnInit(){
     this.reactiveForm = new FormGroup({
       personalDetails : new FormGroup({
-        firstname: new FormControl(null, Validators.required),
-        lastname: new FormControl(null, Validators.required),
-        email: new FormControl(null, [Validators.required, Validators.email]),
+        firstname: new FormControl(null, [Validators.required, this.noSpaceAllowed]),
+        lastname: new FormControl(null, [Validators.required, this.noSpaceAllowed]),
+        email: new FormControl(null, [Validators.required, Validators.email], this.emailNotAllowed),
       }),   
       gender: new FormControl('male'),
       country: new FormControl('india'),
-      hobbies: new FormControl(null)
+      hobbies: new FormControl(null),
+      skills: new FormArray([
+        new FormControl(null, Validators.required),
+      ])
     });
   }
 
   onSubmit(){
     console.log(this.reactiveForm);
+  }
+
+  addSklls(){
+    (<FormArray>this.reactiveForm.get('skills')).push(new FormControl(null, Validators.required));
+  }
+
+  noSpaceAllowed(control: FormControl){
+    if(control.value != null && control.value.indexOf(' ') != -1){
+      return {noSpaceAllowed: true}
+    }
+    return null;
+  }
+
+  //Create async Validator
+  emailNotAllowed(control: FormControl): Promise<any> | Observable<any> {
+    const response = new Promise((resolve,reject)=>{
+      setTimeout(()=>{
+        if(control.value === 'procademy@gmail.com'){
+          resolve({emailNotAllowed: true})
+        } else {
+          resolve(null)
+        }
+      }, 5000)
+    });
+    return response;
   }
 }
